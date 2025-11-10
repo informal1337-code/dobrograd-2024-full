fdaff14b23b3554f4174b84e56fcc4775b902760
informal <inf0rmal1337@outlook.com>
2025-11-10 19:51:27 +0300

    binds additional

--- addons/core-octolib/lua/octolib/modules/bind/bind/shared.lua
+++ addons/core-octolib/lua/octolib/modules/bind/bind/shared.lua
@@ -3,3 +3,7 @@ octolib.bind = octolib.bind or {}
 
 octolib.client('vgui/octolib_bind_panel')
 octolib.client('vgui/octolib_bind_row')
+octolib.client('client')
+
+octolib.server('server')
+octolib.server('init')
