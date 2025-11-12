b2a5d0285fdfc3ccfe6c79beb88c4b2685e5969f
informal <inf0rmal1337@outlook.com>
2025-11-12 19:36:37 +0300

    ФИКС КРАШЕЙ

--- addons/core-octolib/lua/octolib/modules/bind/init.lua
+++ addons/core-octolib/lua/octolib/modules/bind/init.lua
@@ -1,4 +1,9 @@
 octolib = octolib or {}
 octolib.bind = octolib.bind or {}
-
+if SERVER then
+    AddCSLuaFile()
+    include('sv_init.lua')
+else
+    include('cl_init.lua')
+end
 octolib.shared('shared')
